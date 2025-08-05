ef4de77b329206062b45158cfcd327b6c49d3597
lightpurple <dmstjd10@gmail.com>
2025-08-05 16:21:45 +0900

    DON-15 환경변수 라이브러리 변경
    - dotenv -> @nestjs/config

---
+++ src/lib/utils.ts
@@ -0,0 +1,9 @@
+import { ConfigService } from "@nestjs/config";
+
+export function getRequiredEnv(config: ConfigService, key: string): string {
+    const value = config.get<string>(key);
+    if (!value) {
+        throw new Error(`${key} 환경변수가 설정되지 않았습니다.`);
+    }
+    return value;
+}
