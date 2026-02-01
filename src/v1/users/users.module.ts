b21a900cd3056b3e01c59325dd8181a9ef62b55c
taeYang <zxcv2987@naver.com>
2026-02-01 16:44:07 +0900

    fix: 동아리 레포 호출을 위한 모듈 추가

--- src/v1/users/users.module.ts
+++ src/v1/users/users.module.ts
@@ -3,10 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
+import { Club } from '../clubs/entities/club.entity';
 import { ClubsModule } from '../clubs/clubs.module';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User]), forwardRef(() => ClubsModule)],
+    imports: [
+        TypeOrmModule.forFeature([User, Club]),
+        forwardRef(() => ClubsModule),
+    ],
     controllers: [UsersController],
     providers: [UsersService],
     exports: [UsersService],
